ce761578b4b5b33ef5c4d8f996e60e7c71f45ba6
metaphorizer <41169415+metaphorizer@users.noreply.github.com>
2018-10-04 23:58:44 +0300

    Delete 20181002230800_add_answers_correct_default_value.rb

--- db/migrate/20181002230800_add_answers_correct_default_value.rb
+++
@@ -1,6 +0,0 @@
-#set default value of correct answer to 'I know it!'
-class AddAnswersCorrectDefaultValue < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:answers, :correct, 'I know it!')
-  end
-end
