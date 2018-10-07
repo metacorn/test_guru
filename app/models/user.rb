1cc3633cd1427fbeadef2c38160905f2a3ebc5fb
Oleg Sachev <cbi444@gmail.com>
2018-10-07 18:31:46 +0300

    Fix test_by_level method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   # Return tests of certain level passed and passing by user.
   def test_by_level(level)
     user_id = self.id
-    PassingTest.joins("JOIN tests ON passing_tests.test_id = tests.id").where("level = :level AND user_id = user_id", level: level, user_id: user_id)    
+    PassingTest.joins("JOIN tests ON passing_tests.test_id = tests.id").where("level = :level AND user_id = :user_id", level: level, user_id: user_id)    
   end
 end
