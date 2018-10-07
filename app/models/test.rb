2802f1207376daa5c7219628419d43d29f91d259
Oleg Sachev <cbi444@gmail.com>
2018-10-07 20:18:27 +0300

    Add aliases for Test and Category 'title' attributes, add Test class method ordered_tests_titles_by_category for returning an array containing titles of tests of certain category

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  alias_attribute :test_title, :title
+
+  def self.ordered_tests_titles_by_category(category_title)
+    category_id = Category.find_by(category_title: category_title).id
+    Test.joins("JOIN categories ON tests.category_id = categories.id").where(category_id: category_id).pluck(:test_title).sort.reverse!
+  end
 end
