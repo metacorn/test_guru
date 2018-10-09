04bc7f1d87b1d2868d624ec96ef2dfbbed44c67f
metaphorizer <41169415+metaphorizer@users.noreply.github.com>
2018-10-09 15:18:59 +0300

    Delete 20181002230748_add_default_values.rb

--- db/migrate/20181002230748_add_default_values.rb
+++
@@ -1,7 +0,0 @@
-#set default test level to 'beginner' (0)
-class AddDefaultValues < ActiveRecord::Migration[5.2]
-  def change
-    change_column_default(:tests, :level, from: nil, to: 0)
-    change_column_default(:answers, :correct, from: nil, to: false)
-  end
-end
