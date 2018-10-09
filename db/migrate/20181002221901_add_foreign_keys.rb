406779ffc702a3942f34ea1c7c256fb49157cc1d
metaphorizer <41169415+metaphorizer@users.noreply.github.com>
2018-10-09 15:18:16 +0300

    Delete 20181002221901_add_foreign_keys.rb

--- db/migrate/20181002221901_add_foreign_keys.rb
+++
@@ -1,9 +0,0 @@
-class AddForeignKeys < ActiveRecord::Migration[5.2]
-  def change
-    add_reference :tests, :category, foreign_key: true
-    add_reference :questions, :test, foreign_key: true
-    add_reference :answers, :question, foreign_key: true
-    add_reference :passing_tests, :user, foreign_key: true
-    add_reference :passing_tests, :test, foreign_key: true
-  end
-end
