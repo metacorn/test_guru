177d2d87d2f1a4d0334b4ae02e4a920b8728806a
Oleg Sachev <cbi444@gmail.com>
2018-10-10 23:58:22 +0300

    Update User#test_by_level method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   def test_by_level(level)
-    Test.joins("JOIN passing_tests ON passing_tests.test_id = tests.id").where("level = :level AND user_id = :user_id", level: level, user_id: id)
+    Test.joins("JOIN passing_tests ON passing_tests.test_id = tests.id").where("tests.level = :level AND passing_tests.user_id = :user_id", level: level, user_id: user_id)
   end
 end
