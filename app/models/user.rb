0debe1dd16e781c37137f691d29284de514de0d1
Oleg Sachev <cbi444@gmail.com>
2018-10-10 12:26:31 +0300

    Add 'belongs_to' associations to Test, Question and Answer. Fix db/seed.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  def test_by_level(level)    
-    PassingTest.joins("JOIN tests ON passing_tests.test_id = tests.id").where("level = :level AND user_id = :user_id", level: level, user_id: id).to_a
+  def test_by_level(level)
+    Test.joins("JOIN passing_tests ON passing_tests.test_id = tests.id").where("level = :level AND user_id = :user_id", level: level, user_id: id)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,159 +24,159 @@ tests = Test.create!([
 ])
 # Creating questions for test with id 1
 questions = Question.create!([
-  { body: 'When was Kievan Rus founded?', test_id: tests[0].id },
-  { body: 'Who did convert Kievan Rus to christianity?', test_id: tests[0].id },
-  { body: 'What was the capital of Kievan Rus?', test_id: tests[0].id },
-  { body: 'By whom was Kievan Rus founded?', test_id: tests[0].id }
+  { body: 'When was Kievan Rus founded?', test: tests[0] },
+  { body: 'Who did convert Kievan Rus to christianity?', test: tests[0] },
+  { body: 'What was the capital of Kievan Rus?', test: tests[0] },
+  { body: 'By whom was Kievan Rus founded?', test: tests[0] }
 ])
 # Creating answers for test with id 1
 answers = Answer.create!([
-  { body: 'In 27 B.C.', question_id: questions[0].id },
-  { body: 'In 882', question_id: questions[0].id, correct: true },
-  { body: 'In 1991', question_id: questions[0].id },
-  { body: 'In 1917', question_id: questions[0].id },
-  { body: 'Jesus Christ', question_id: questions[1].id },
-  { body: 'Vladimir Putin', question_id: questions[1].id },
-  { body: 'Salah ad-Din Yusuf', question_id: questions[1].id },
-  { body: 'Vladimir the Great', question_id: questions[1].id, correct: true},
-  { body: 'Chernigov', question_id: questions[2].id },
-  { body: 'Kiev', question_id: questions[2].id, correct: true },
-  { body: 'Moscow', question_id: questions[2].id },
-  { body: 'Balashiha', question_id: questions[2].id },
-  { body: 'Igor', question_id: questions[3].id },
-  { body: 'Rurik', question_id: questions[3].id },
-  { body: 'Oleg', question_id: questions[3].id, correct: true },
-  { body: 'Olga', question_id: questions[3].id }
+  { body: 'In 27 B.C.', question: questions[0] },
+  { body: 'In 882', question: questions[0], correct: true },
+  { body: 'In 1991', question: questions[0] },
+  { body: 'In 1917', question: questions[0] },
+  { body: 'Jesus Christ', question: questions[1] },
+  { body: 'Vladimir Putin', question: questions[1] },
+  { body: 'Salah ad-Din Yusuf', question: questions[1] },
+  { body: 'Vladimir the Great', question: questions[1], correct: true},
+  { body: 'Chernigov', question: questions[2] },
+  { body: 'Kiev', question: questions[2], correct: true },
+  { body: 'Moscow', question: questions[2] },
+  { body: 'Balashiha', question: questions[2] },
+  { body: 'Igor', question: questions[3] },
+  { body: 'Rurik', question: questions[3] },
+  { body: 'Oleg', question: questions[3], correct: true },
+  { body: 'Olga', question: questions[3] }
 ])
 # Creating questions for test with id 2
 questions = Question.create!([
-  { body: 'The average lifespan during the middle ages was what?', test_id: tests[1].id },
-  { body: 'What was created to limit the king\'s power in England', test_id: tests[1].id },
-  { body: 'Which international event was started by Pope Urban II\'s sermon at Clermont in 1095?', test_id: tests[1].id },
-  { body: 'What proportion of Europe\'s population is estimated to have been killed by the Black Death in the 14th century?', test_id: tests[1].id }
+  { body: 'The average lifespan during the middle ages was what?', test: tests[1] },
+  { body: 'What was created to limit the king\'s power in England', test: tests[1] },
+  { body: 'Which international event was started by Pope Urban II\'s sermon at Clermont in 1095?', test: tests[1] },
+  { body: 'What proportion of Europe\'s population is estimated to have been killed by the Black Death in the 14th century?', test: tests[1] }
 ])
 # Creating answers for test with id 2
 answers = Answer.create!([
-  { body: '20', question_id: questions[0].id },
-  { body: '75', question_id: questions[0].id },
-  { body: '35', question_id: questions[0].id, correct: true },
-  { body: '50', question_id: questions[0].id },
-  { body: 'Constitution', question_id: questions[1].id },
-  { body: 'Magna Carta', question_id: questions[1].id, correct: true },
-  { body: 'Shay\'s Rebellion', question_id: questions[1].id },
-  { body: 'Bill of Rights', question_id: questions[1].id },
-  { body: 'The Great Schism', question_id: questions[2].id },
-  { body: 'The Hundred Years\' War', question_id: questions[2].id },
-  { body: 'The Peace of God movement', question_id: questions[2].id },
-  { body: 'The First Crusade', question_id: questions[2].id, correct: true },
-  { body: '10-15%', question_id: questions[3].id },
-  { body: 'About 95%', question_id: questions[3].id },
-  { body: '30-60%', question_id: questions[3].id, correct: true },
-  { body: 'Nobody was killed', question_id: questions[3].id }
+  { body: '20', question: questions[0] },
+  { body: '75', question: questions[0] },
+  { body: '35', question: questions[0], correct: true },
+  { body: '50', question: questions[0] },
+  { body: 'Constitution', question: questions[1] },
+  { body: 'Magna Carta', question: questions[1], correct: true },
+  { body: 'Shay\'s Rebellion', question: questions[1] },
+  { body: 'Bill of Rights', question: questions[1] },
+  { body: 'The Great Schism', question: questions[2] },
+  { body: 'The Hundred Years\' War', question: questions[2] },
+  { body: 'The Peace of God movement', question: questions[2] },
+  { body: 'The First Crusade', question: questions[2], correct: true },
+  { body: '10-15%', question: questions[3] },
+  { body: 'About 95%', question: questions[3] },
+  { body: '30-60%', question: questions[3], correct: true },
+  { body: 'Nobody was killed', question: questions[3] }
 ])
 # Creating questions for test with id 3
 questions = Question.create!([
-  { body: 'Which two actors directed themselves in movies and won Oscars for Best Actor?', test_id: tests[2].id },
-  { body: '"After all, tomorrow is another day!" was the last line in which Oscar-winning Best Picture?', test_id: tests[2].id },
-  { body: 'Who is the only person to win an Oscar for Best Director for the only movie he ever directed?', test_id: tests[2].id },
-  { body: 'Who is the most nominated actor in Academy history?', test_id: tests[2].id }
+  { body: 'Which two actors directed themselves in movies and won Oscars for Best Actor?', test: tests[2] },
+  { body: '"After all, tomorrow is another day!" was the last line in which Oscar-winning Best Picture?', test: tests[2] },
+  { body: 'Who is the only person to win an Oscar for Best Director for the only movie he ever directed?', test: tests[2] },
+  { body: 'Who is the most nominated actor in Academy history?', test: tests[2] }
 ])
 # Creating answers for test with id 3
 answers = Answer.create!([
-  { body: 'Al Pacino and Timothy Hutton', question_id: questions[0].id },
-  { body: 'Jack Nicholson and Kevin Spacey', question_id: questions[0].id },
-  { body: 'Laurence Olivier and Roberto Benigni', question_id: questions[0].id, correct: true },
-  { body: 'Tom Hanks and Paul Newman', question_id: questions[0].id },
-  { body: 'Gone With the Wind', question_id: questions[1].id, correct: true },
-  { body: 'Great Expectations', question_id: questions[1].id },
-  { body: 'Harold and Maude', question_id: questions[1].id },
-  { body: 'The Matrix', question_id: questions[1].id },
-  { body: 'Bob Fosse', question_id: questions[2].id },
-  { body: 'Frank Borzage', question_id: questions[2].id },
-  { body: 'Leo McCarey', question_id: questions[2].id },
-  { body: 'Jerome Robbins', question_id: questions[2].id, correct: true },
-  { body: 'Jack Nicholson', question_id: questions[3].id, correct: true },
-  { body: 'Laurence Olivier', question_id: questions[3].id },
-  { body: 'Spencer Tracy', question_id: questions[3].id },
-  { body: 'Paul Newman', question_id: questions[3].id }
+  { body: 'Al Pacino and Timothy Hutton', question: questions[0] },
+  { body: 'Jack Nicholson and Kevin Spacey', question: questions[0] },
+  { body: 'Laurence Olivier and Roberto Benigni', question: questions[0], correct: true },
+  { body: 'Tom Hanks and Paul Newman', question: questions[0] },
+  { body: 'Gone With the Wind', question: questions[1], correct: true },
+  { body: 'Great Expectations', question: questions[1] },
+  { body: 'Harold and Maude', question: questions[1] },
+  { body: 'The Matrix', question: questions[1] },
+  { body: 'Bob Fosse', question: questions[2] },
+  { body: 'Frank Borzage', question: questions[2] },
+  { body: 'Leo McCarey', question: questions[2] },
+  { body: 'Jerome Robbins', question: questions[2], correct: true },
+  { body: 'Jack Nicholson', question: questions[3], correct: true },
+  { body: 'Laurence Olivier', question: questions[3] },
+  { body: 'Spencer Tracy', question: questions[3] },
+  { body: 'Paul Newman', question: questions[3] }
 ])
 # Creating questions for test with id 4
 questions = Question.create!([
-  { body: 'Austrian born director Fritz Lang started his film-making career in Germany during the Weimar Republic Era. After fleeing Nazi Germany, he continued his thread of film noir movies from Hollywood. Which of the following epic movies is NOT one of his movies?', test_id: tests[3].id },
-  { body: 'British actor Daniel Craig, now a household name synonymous with the "James Bond" franchise, made his first appearance as Bond in which film, released worldwide in 2006?', test_id: tests[3].id },
-  { body: 'Which of these movies is set in Wales and stars Hugh Grant as cartographer Reginald Anson?', test_id: tests[3].id },
-  { body: 'Which of these actors was born in New York and played Mark Zuckerberg in "The Social Network"?', test_id: tests[3].id }
+  { body: 'Austrian born director Fritz Lang started his film-making career in Germany during the Weimar Republic Era. After fleeing Nazi Germany, he continued his thread of film noir movies from Hollywood. Which of the following epic movies is NOT one of his movies?', test: tests[3] },
+  { body: 'British actor Daniel Craig, now a household name synonymous with the "James Bond" franchise, made his first appearance as Bond in which film, released worldwide in 2006?', test: tests[3] },
+  { body: 'Which of these movies is set in Wales and stars Hugh Grant as cartographer Reginald Anson?', test: tests[3] },
+  { body: 'Which of these actors was born in New York and played Mark Zuckerberg in "The Social Network"?', test: tests[3] }
 ])
 # Creating answers for test with id 4
 answers = Answer.create!([
-  { body: 'Metropolis', question_id: questions[0].id, correct: true },
-  { body: 'Fury', question_id: questions[0].id },
-  { body: 'M', question_id: questions[0].id },
-  { body: 'Dial M for Murder', question_id: questions[0].id },
-  { body: 'Quantum of Solace', question_id: questions[1].id },
-  { body: 'Die Another Day', question_id: questions[1].id },
-  { body: 'Skyfall', question_id: questions[1].id },
-  { body: 'Casino Royale', question_id: questions[1].id, correct: true },
-  { body: 'The Lair of the White Worm', question_id: questions[2].id },
-  { body: 'Cloud Atlas', question_id: questions[2].id },
-  { body: 'Notting Hill', question_id: questions[2].id },
-  { body: 'The Englishman Who Went Up a Hill But Came Down a Mountain', question_id: questions[2].id, correct: true },
-  { body: 'Andrew Garfield', question_id: questions[3].id },
-  { body: 'Jesse Eisenberg', question_id: questions[3].id, correct: true },
-  { body: 'Ben Affleck', question_id: questions[3].id },
-  { body: 'Bradley Cooper', question_id: questions[3].id }
+  { body: 'Metropolis', question: questions[0], correct: true },
+  { body: 'Fury', question: questions[0] },
+  { body: 'M', question: questions[0] },
+  { body: 'Dial M for Murder', question: questions[0] },
+  { body: 'Quantum of Solace', question: questions[1] },
+  { body: 'Die Another Day', question: questions[1] },
+  { body: 'Skyfall', question: questions[1] },
+  { body: 'Casino Royale', question: questions[1], correct: true },
+  { body: 'The Lair of the White Worm', question: questions[2] },
+  { body: 'Cloud Atlas', question: questions[2] },
+  { body: 'Notting Hill', question: questions[2] },
+  { body: 'The Englishman Who Went Up a Hill But Came Down a Mountain', question: questions[2], correct: true },
+  { body: 'Andrew Garfield', question: questions[3] },
+  { body: 'Jesse Eisenberg', question: questions[3], correct: true },
+  { body: 'Ben Affleck', question: questions[3] },
+  { body: 'Bradley Cooper', question: questions[3] }
 ])
 # Creating questions for test with id 5
 questions = Question.create!([
-  { body: 'In what year did England win their first and only World Cup?', test_id: tests[4].id },
-  { body: 'Which country has won the World Cup the most times?', test_id: tests[4].id },
-  { body: 'Team of which country is the actual owner of the World Cup?', test_id: tests[4].id },
-  { body: 'Which country won the first World Cup?', test_id: tests[4].id }
+  { body: 'In what year did England win their first and only World Cup?', test: tests[4] },
+  { body: 'Which country has won the World Cup the most times?', test: tests[4] },
+  { body: 'Team of which country is the actual owner of the World Cup?', test: tests[4] },
+  { body: 'Which country won the first World Cup?', test: tests[4] }
 ])
 # Creating answers for test with id 5
 answers = Answer.create!([
-  { body: '1966', question_id: questions[0].id, correct: true },
-  { body: '1994', question_id: questions[0].id },
-  { body: '2002', question_id: questions[0].id },
-  { body: '2018', question_id: questions[0].id },
-  { body: 'Germany', question_id: questions[1].id },
-  { body: 'Spain', question_id: questions[1].id },
-  { body: 'Brazil', question_id: questions[1].id, correct: true },
-  { body: 'France', question_id: questions[1].id },
-  { body: 'Portugal', question_id: questions[2].id },
-  { body: 'France', question_id: questions[2].id, correct: true },
-  { body: 'Croatia', question_id: questions[2].id },
-  { body: 'Russia', question_id: questions[2].id },
-  { body: 'Uruguay', question_id: questions[3].id, correct: true },
-  { body: 'Brazil', question_id: questions[3].id },
-  { body: 'Italy', question_id: questions[3].id },
-  { body: 'Germany', question_id: questions[3].id },
+  { body: '1966', question: questions[0], correct: true },
+  { body: '1994', question: questions[0] },
+  { body: '2002', question: questions[0] },
+  { body: '2018', question: questions[0] },
+  { body: 'Germany', question: questions[1] },
+  { body: 'Spain', question: questions[1] },
+  { body: 'Brazil', question: questions[1], correct: true },
+  { body: 'France', question: questions[1] },
+  { body: 'Portugal', question: questions[2] },
+  { body: 'France', question: questions[2], correct: true },
+  { body: 'Croatia', question: questions[2] },
+  { body: 'Russia', question: questions[2] },
+  { body: 'Uruguay', question: questions[3], correct: true },
+  { body: 'Brazil', question: questions[3] },
+  { body: 'Italy', question: questions[3] },
+  { body: 'Germany', question: questions[3] },
 ])
 # Creating questions for test with id 6
 questions = Question.create!([
-  { body: 'What team does Elton John support and own?', test_id: tests[5].id },
-  { body: 'Which German club has the largest average attendance?', test_id: tests[5].id },
-  { body: 'Fans of which Italian club are called "Irriducibili"?', test_id: tests[5].id },
-  { body: 'Fans of which team come together at the forum named "Landskrona"?', test_id: tests[5].id }
+  { body: 'What team does Elton John support and own?', test: tests[5] },
+  { body: 'Which German club has the largest average attendance?', test: tests[5] },
+  { body: 'Fans of which Italian club are called "Irriducibili"?', test: tests[5] },
+  { body: 'Fans of which team come together at the forum named "Landskrona"?', test: tests[5] }
 ])
 # Creating answers for test with id 6
 answers = Answer.create!([
-  { body: 'Coventry City', question_id: questions[0].id },
-  { body: 'Manchester City', question_id: questions[0].id },
-  { body: 'Wimbledon', question_id: questions[0].id },
-  { body: 'Watford F.C.', question_id: questions[0].id, correct: true },
-  { body: 'Herta', question_id: questions[1].id },
-  { body: 'Borussia Dortmund', question_id: questions[1].id, correct: true },
-  { body: 'Bayern', question_id: questions[1].id },
-  { body: 'Hannover', question_id: questions[1].id },
-  { body: 'Internazionale', question_id: questions[2].id },
-  { body: 'Fiorentina', question_id: questions[2].id },
-  { body: 'Lazio', question_id: questions[2].id, correct: true },
-  { body: 'Torino', question_id: questions[2].id },
-  { body: 'Wisla', question_id: questions[3].id },
-  { body: 'Gottenburg', question_id: questions[3].id },
-  { body: 'Partizan', question_id: questions[3].id },
-  { body: 'Zenit', question_id: questions[3].id, correct: true }
+  { body: 'Coventry City', question: questions[0] },
+  { body: 'Manchester City', question: questions[0] },
+  { body: 'Wimbledon', question: questions[0] },
+  { body: 'Watford F.C.', question: questions[0], correct: true },
+  { body: 'Herta', question: questions[1] },
+  { body: 'Borussia Dortmund', question: questions[1], correct: true },
+  { body: 'Bayern', question: questions[1] },
+  { body: 'Hannover', question: questions[1] },
+  { body: 'Internazionale', question: questions[2] },
+  { body: 'Fiorentina', question: questions[2] },
+  { body: 'Lazio', question: questions[2], correct: true },
+  { body: 'Torino', question: questions[2] },
+  { body: 'Wisla', question: questions[3] },
+  { body: 'Gottenburg', question: questions[3] },
+  { body: 'Partizan', question: questions[3] },
+  { body: 'Zenit', question: questions[3], correct: true }
 ])
 # Users seeding.
 User.create(user_class: 1, username: 'administrator', password: 'no_figure_out', email: 'administrator@gmail.com')
