d98beb35a7f54d1f95de23cc0424c94b08b0e85b
Oleg Sachev <cbi444@gmail.com>
2018-10-12 16:02:33 +0300

    Add association between Test and User through PassingTest

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,8 @@ class Test < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :questions
+  has_many :passing_tests
+  has_many :users through: :passing_tests
 
   def self.ordered_tests_titles_by_category(category_title)
     joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :tests
+  has_many :passing_tests
+  has_many :tests through: :passing_tests
 
   def test_by_level(level)
     Test.joins("JOIN passing_tests ON passing_tests.test_id = tests.id").where("tests.level = :level AND passing_tests.user_id = :user_id", level: level, user_id: user_id)
