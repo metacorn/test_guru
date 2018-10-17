4c6bfb5b0ec27ddbc8c2299c003c32eb3e20c354
Oleg Sachev <cbi444@gmail.com>
2018-10-17 17:04:41 +0300

    Add scope method for returning tests by certain category

--- app/models/test.rb
+++ app/models/test.rb
@@ -11,6 +11,7 @@ class Test < ApplicationRecord
   scope :easy, -> { where(level: 0..1) }
   scope :medium, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :by_category, -> (category) { joins(:category).where(categories: { title: category }) }
 
   def self.ordered_tests_titles_by_category(category_title)
     joins(:category).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
