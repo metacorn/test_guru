893c6c53b8fa1e894e07db1eaa18bf9025032315
Oleg Sachev <cbi444@gmail.com>
2018-10-31 23:25:13 +0300

    Add rescue_with_question_not_found method to questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,8 @@ class QuestionsController < ApplicationController
   before_action :get_test, only: %i[index]
   before_action :get_question, only: %i[show destroy]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     questions = @test.questions.pluck(:body)
     render plain: questions.join("\n")
@@ -39,5 +41,9 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:body)
+  end
+
+  def rescue_with_question_not_found
+    render plain: "This question was not found!"
   end
 end
