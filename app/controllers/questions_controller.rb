f81e06781b0f05a9fb278f5afa4d54b88444f886
Oleg Sachev <cbi444@gmail.com>
2018-10-31 21:51:43 +0300

    Add show method to question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,23 @@
 class QuestionsController < ApplicationController
   before_action :get_test, only: %i[index]
+  before_action :get_question, only: %i[show]
 
   def index
     questions = @test.questions.pluck(:body)
     render plain: questions.join("\n")
   end
 
+  def show
+    render plain: @question.body
+  end
+
   private
 
   def get_test
     @test = Test.find(params[:test_id])
+  end
+
+  def get_question
+    @question = Question.find(params[:id])
   end
 end
