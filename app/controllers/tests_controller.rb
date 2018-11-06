7b1b1698a210222522d8e0a2b07b71aedf2ab291
Oleg Sachev <cbi444@gmail.com>
2018-11-07 01:50:25 +0300

    Add show method for tests controller

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,4 +1,17 @@
 class TestsController < ApplicationController
+  before_action :get_test, only: %i[show]
+  before_action :get_questions, only: %i[show]
+
   def show
+  end
+
+  private
+
+  def get_test
+    @test = Test.find(params[:id])
+  end
+
+  def get_questions
+    @questions = @test.questions
   end
 end
