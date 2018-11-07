aefdb6e5f544c60b96f3e478dbe280b67c4dad9f
Oleg Sachev <cbi444@gmail.com>
2018-11-08 00:06:58 +0300

    Update new view for questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_action :get_test, only: %i[index create]
+  before_action :get_test, only: %i[index create new]
   before_action :get_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
@@ -13,6 +13,7 @@ class QuestionsController < ApplicationController
   end
 
   def new
+    @question = Question.new
   end
 
   def create
