94c392b9510e0988bd445e34067743468ea07c97
Oleg Sachev <cbi444@gmail.com>
2019-05-16 19:01:04 +0300

    Fix QuestionsController#question_params.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -47,7 +47,7 @@ class QuestionsController < ApplicationController
   end
 
   def question_params
-    params.require(:question).permit(:body, :test_id)
+    params.require(:question).permit(:body)
   end
 
   # def rescue_with_question_not_found
