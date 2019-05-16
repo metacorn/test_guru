9cbb2600063db17f455c2d7aad2d3dfa7267a8fa
Oleg Sachev <cbi444@gmail.com>
2019-05-16 23:04:23 +0300

    Add helper for GitHub URL.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def actual_year
     Time.now.year
   end
+
+  def github_url(author, repo)
+    link = 'https://github.com/' + author + '/' + repo
+    link_to 'GitHub', link
+  end
 end
