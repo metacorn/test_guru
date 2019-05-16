4ef0e5957c41359c4a429930bea8f01856d04b91
Oleg Sachev <cbi444@gmail.com>
2019-05-16 23:24:48 +0300

    Add helper for Questions form header.

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def question_header(question, test)
+    if question.body.nil?
+      "Create new #{test.title} question:"
+    else
+      "Edit #{test.title} Question:"
+    end
+  end
 end
