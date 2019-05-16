27630e0f26f86c6ab4100e2faa65bb4ac95a1e0c
Oleg Sachev <cbi444@gmail.com>
2019-05-16 23:49:17 +0300

    Add index action for Tests with showing number of questions for each test.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,6 +2,10 @@ class TestsController < ApplicationController
   before_action :get_test, only: %i[show]
   before_action :get_questions, only: %i[show]
 
+  def index
+    @tests = Test.all
+  end
+
   def show
   end
 
