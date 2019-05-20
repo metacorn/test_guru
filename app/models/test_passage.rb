9723700a08d6776f244bcdb10f0e09a470fbd9ce
Oleg Sachev <cbi444@gmail.com>
2019-05-20 16:17:33 +0300

    Refactor callbacks in TestPassage model.

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,8 +3,7 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_update :before_update_next_question
+  before_validation :before_validation_set_current_question
 
   def completed?
     current_question.nil?
@@ -31,13 +30,17 @@ class TestPassage < ApplicationRecord
   end
 
   def remaining_questions
-    test.questions.order(:id).where('id > ?', current_question.id)
+    if current_question.nil?
+      test.questions.order(:id)
+    else
+      test.questions.order(:id).where('id > ?', current_question.id)
+    end
   end
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def before_validation_set_current_question
+    self.current_question = remaining_questions.first
   end
 
   def correct_answer?(answer_ids)
@@ -47,9 +50,5 @@ class TestPassage < ApplicationRecord
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def before_update_next_question
-    self.current_question = remaining_questions.first
   end
 end
