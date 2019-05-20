15b3659049a9066888796793d376e1c3b2a4b09b
Oleg Sachev <cbi444@gmail.com>
2019-05-20 22:31:43 +0300

    Add accessors for passwords attributes for Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+
+  attr_reader :password
+  attr_writer :password_confirmation
+
   has_many :created_tests, class_name: 'Test'
   has_many :test_passages
   has_many :tests, through: :test_passages
@@ -17,6 +21,14 @@ class User < ApplicationRecord
 
   def authentificate(password_string)
     digest(password_string) == self.password_digest ? self : false
+  end
+
+  def password=(password_string)
+    if password_string.nil?
+      self.password_digest = nil
+    elsif password_string.present?
+      @password = password_string
+      self.password_digest = digest(password_string)
   end
 
   private
