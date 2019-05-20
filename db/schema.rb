f5f8f7aadc3d5b82c59388d81ea858651f0ad585
Oleg Sachev <cbi444@gmail.com>
2019-05-20 16:48:38 +0300

    Generate migration for adding password_digest attribute to Users.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_18_212647) do
+ActiveRecord::Schema.define(version: 2019_05_20_134715) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2019_05_18_212647) do
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
