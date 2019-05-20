1ff169f06fc58da22a099ab51384717ea1a2bf4e
Oleg Sachev <cbi444@gmail.com>
2019-05-20 21:46:43 +0300

    Add user_params method to Users controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,10 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
