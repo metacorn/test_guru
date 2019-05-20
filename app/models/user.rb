a2a396cff8ffcbffcce092aa70b9f7b8055bc5b8
Oleg Sachev <cbi444@gmail.com>
2019-05-20 16:56:15 +0300

    Add authentificate and digest methods to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   has_many :created_tests, class_name: 'Test'
   has_many :test_passages
@@ -11,5 +13,15 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
+  end
+
+  def authentificate(password_string)
+    digest(password_string) == self.password_digest ? self : false
+  end
+
+  private
+
+  def digest(string)
+    Digest::SHA1.hexdigest(string)
   end
 end
