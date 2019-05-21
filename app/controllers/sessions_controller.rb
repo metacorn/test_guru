7162ac49839a53e18e16b75ef7f47e36d729b180
Oleg Sachev <cbi444@gmail.com>
2019-05-21 12:10:40 +0300

    Fix create action of Sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to test_path
+      redirect_to tests_path
     else
       render :new
     end
