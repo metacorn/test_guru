d3001bad88efd071078559c1ffd6fd2e5cd7d6f6
Oleg Sachev <cbi444@gmail.com>
2019-05-21 12:32:26 +0300

    Add flash messages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
   def authenticate_user!
     unless current_user
       redirect_to login_path, alert: 'Are you a Guru? Verify your email and password please.'
-      cookies[:path] = request.path
     end
   end
 
