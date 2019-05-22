e16d27ce69682e4cea7d75413ea9b3e0f3a86905
Oleg Sachev <cbi444@gmail.com>
2019-05-22 23:09:43 +0300

    Fix migrations.

--- db/migrate/20190520191231_remove_fields_from_users.rb
+++ db/migrate/20190520191231_remove_fields_from_users.rb
@@ -1,25 +1,13 @@
 class RemoveFieldsFromUsers < ActiveRecord::Migration[5.2]
   def up
     remove_column :users, :password
-  end
-
-  def down
-    add_column :users, :password, :string
-  end
-
-  def up
+    remove_column :users, :username
     remove_column :users, :user_class
   end
 
   def down
-    add_column :users, :user_class, :integer
-  end
-
-  def up
-    remove_column :users, :username
-  end
-
-  def down
+    add_column :users, :password, :string
     add_column :users, :username, :string
+    add_column :users, :user_class, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -60,8 +60,6 @@ ActiveRecord::Schema.define(version: 2019_05_22_193950) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "user_class", null: false
-    t.string "password", null: false
     t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
