9f8c9e375fa9c8d8c0b5330f7be0f760b28a2080
Oleg Sachev <cbi444@gmail.com>
2019-05-28 17:41:20 +0300

    Stylize flash messages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     link = "https://github.com/#{author}/#{repo}"
     link_to 'GitHub', link, target: "_blank"
   end
+
+  def flash_class(key)
+    case key
+    when :alert
+      "alert alert-danger"
+    when :notice
+      "alert alert-success"
+    else
+      "alert alert-primary"
+    end
+  end
 end
