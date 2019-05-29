1c1899f76d1d4db92203d7d1561bc99e5db8c899
Oleg Sachev <cbi444@gmail.com>
2019-05-29 10:47:44 +0300

    Update GitHubClient class.

--- lib/clients/git_hub_client.rb
+++ lib/clients/git_hub_client.rb
@@ -1,9 +1,18 @@
 class GitHubClient
 
   ROOT_ENDPOINT = "https://api.github.com"
+  ACCESS_TOKEN = "175c8253d54e25160a610b2f39878144e8c69390"
 
   def initialize
     @http_client = setup_http_client
+  end
+
+  def create_gist(params)
+    @http_client.post("gists") do |request|
+      request.headers["Authorization"] = "token #{ACCESS_TOKEN}"
+      request.headers["Content-Type"] = "application/json"
+      request.body = params.to_json
+    end
   end
 
   private
