4941cc34c9f99280bb4f376a9ae6336e8f6f3ebb
Oleg Sachev <cbi444@gmail.com>
2019-05-29 16:46:22 +0300

    Add ru and en localizations for GistQuestionService.

--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -14,7 +14,7 @@ class GistQuestionService
 
   def gist_params
     {
-      "description": "A question about #{@test.title} from TestGuru",
+      "description": I18n.t(".services.gist_question_service.description", title: @test.title),
       "public": true,
       "files": {
         "test-guru-question.txt": {
