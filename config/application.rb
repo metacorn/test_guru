221bf0800bb90bb1a82f96d1e4ee66ea1d3657cd
Oleg Sachev <cbi444@gmail.com>
2019-05-29 10:25:06 +0300

    Add GitHubClient class, add to autoload_paths.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module TestGuru
     config.load_defaults 5.2
     config.time_zone = "Moscow"
     config.i18n.default_locale = :en
+    config.autoload_paths << "#{Rails.root}/lib/clients"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
