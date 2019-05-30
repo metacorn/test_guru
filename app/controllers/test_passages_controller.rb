549611c61ff40fcf99702ced7c0a204b19e312b2
Oleg Sachev <cbi444@gmail.com>
2019-05-30 14:37:10 +0300

    Update GistQuestionService. Update TestPassages controller.

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -21,9 +21,10 @@ class TestPassagesController < ApplicationController
   end
 
   def gist
-    result = GistQuestionService.new(@test_passage.current_question).call
+    service = GistQuestionService.new(@test_passage.current_question)
+    result = service.call
 
-    if result.url.present?
+    if service.success?
       current_user.gists.create(question: @test_passage.current_question, url: result.html_url)
       flash_options = { notice: t('.success', gist_html_url: result.html_url) }
     else
--- app/services/gist_question_service.rb
+++ app/services/gist_question_service.rb
@@ -10,6 +10,10 @@ class GistQuestionService
     @client.create_gist(gist_params)
   end
 
+  def success?
+    @client.last_response
+  end
+
   private
 
   def gist_params
