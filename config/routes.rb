5faea45416625bf5ed13691da3d31e49e5e7644a
Oleg Sachev <cbi444@gmail.com>
2019-06-02 16:28:18 +0300

    Add Feedback controller. Update routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
+  
   root to: 'tests#index'
+  get '/feedback', to: 'feedback#index'
 
   devise_for :users,  path: :gurus,
                       path_names: { sign_in: :login, sign_out: :logout }
-                      # controllers: {registrations:"registrations"}
 
   resources :tests, only: :index do
     post :start, on: :member
