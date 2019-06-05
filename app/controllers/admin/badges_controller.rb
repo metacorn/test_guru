3181c57fccd57775e120f42aa5b99a93d377d75c
Oleg Sachev <cbi444@gmail.com>
2019-06-05 21:48:04 +0300

    Update Badges controller.

--- app/controllers/admin/badges_controller.rb
+++ app/controllers/admin/badges_controller.rb
@@ -1,4 +1,7 @@
 class Admin::BadgesController < Admin::BaseController
+  before_action :set_badges, only: %i[index]
+  before_action :set_badge, only: %i[show destroy]
+
   def index
   end
 
@@ -11,12 +14,27 @@ class Admin::BadgesController < Admin::BaseController
 
   def create
     @badge = Badge.new(badge_params)
+    if @badge.save
+      redirect_to [:admin, @badge], notice: t('.success')
+    else
+      render :new
+    end
   end
 
   def destroy
+    @badge.destroy
+    redirect_to admin_badges_path, notice: t('.success')
   end
 
   private
+
+  def set_badges
+    @badges = Badge.all
+  end
+
+  def set_badge
+    @badge = Badge.find(params[:id])
+  end  
 
   def badge_params
     params.require(:badge).permit(:title, :image, :rule_type, :rule_value)
