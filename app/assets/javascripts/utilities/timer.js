46d5adfd88c2d1a91328f79237ea52eebcc0ea37
Oleg Sachev <cbi444@gmail.com>
2019-06-08 13:18:15 +0300

    Fix timer.js.

--- app/assets/javascripts/utilities/timer.js
+++ app/assets/javascripts/utilities/timer.js
@@ -4,23 +4,22 @@ document.addEventListener('turbolinks:load', function() {
   if (timer) {
     remaining_time = parseInt(timer.getAttribute("data"), 10)
     console.log(remaining_time)
-    setInterval(updateTimer, 1000)
+    let updateTimer = setInterval(function () {  
+      minutes = Math.floor(remaining_time / 60)
+      seconds = remaining_time - minutes * 60
+      timer.textContent = minutes + " min " + seconds + " sec left!"
+      remaining_time--
+      if (remaining_time < 0) {
+        clearInterval(updateTimer)
+        timer.textContent = "Time is over! Redirecting to result page..."
+        var nextButton = document.querySelector('#next_button')
+        nextButton.disabled = true
+        setTimeout(function() {
+          nextButton.disabled = false
+          nextButton.click()
+        }, 2000)
+      }
+    }, 1000)
   }
 })
 
-function updateTimer() {  
-  minutes = Math.floor(remaining_time / 60)
-  seconds = remaining_time - minutes * 60
-  timer.textContent = minutes + " min " + seconds + " sec left!"
-  remaining_time--
-  if (remaining_time < 0) {
-    clearInterval()
-    timer.textContent = "Time is over! Redirecting to result page..."
-    var nextButton = document.querySelector('#next_button')
-    nextButton.disabled = true
-    setTimeout(function() {
-      nextButton.disabled = false
-      nextButton.click()
-    }, 2000)
-  }
-}
